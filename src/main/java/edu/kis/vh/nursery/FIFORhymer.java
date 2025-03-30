d136ad443308238c38b2704a66f9acabdf930d28
Filip Juszczak <juszczak.filip@wp.pl>
2025-03-30 22:33:48 +0200

    17. Dodanie dokumentacji klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,19 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa tworzy stos first in first out
+ * dziedziczy ona od klasy DefaultCountingOutRhymer i nadpisuje metodę countOut()
+ *
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Nadpisuje metodę countOut() z klasy bazowej
+     * Elementy są zdejmowane w kolejności, w jakiej zostały dodane, bez odwracania tej kolejności.
+     * @return Zwraca element z przodu stosu FIFO
+     */
     @Override
     public int countOut() {
         while (!callCheck())
@@ -16,6 +26,10 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
 
         return ret;
     }
+    /**
+     * Zwraca referencję do obiektu tymczasowego stosu
+     * @return Obiekt tymczasowego stosu typu DefaultCountingOutRhymer.
+     */
 
     public DefaultCountingOutRhymer getTemp() {
         return temp;
